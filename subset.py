a28efb0441792b5ec4f6fb052226a2d232e24135
SteephanSteve <40833438+SteephanSteve@users.noreply.github.com>
2018-07-14 01:01:05 +0530

    Add files via upload

---
+++ subset.py
@@ -0,0 +1,22 @@
+#!usr/bin/python
+
+n=int(input("N :"))
+m=int(input("M :"))
+a=[]
+b=[]
+c=0
+print "A :"
+for i in range(n):
+      x=int(input())
+      a.append(x)
+print "B :"      
+for i in range(m):
+      x=int(input())
+      b.append(x)
+for i in range(m):
+      if b[i] not in a:
+        c+=1
+if c==0:
+   print "YES"
+else:
+   print "NO"
