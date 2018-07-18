8bb4513949fa8baf684d55d7e0774eaa98910c80
SteephanSteve <40833438+SteephanSteve@users.noreply.github.com>
2018-07-18 19:27:49 +0530

    Update yes.py

--- yes.py
+++ yes.py
@@ -1,9 +1,9 @@
 #!usr/bin/python
 
 n=int(input("N :"))
-a=int(input("A :")
-b=int(input("B :")
-if n%2==0:
+a=int(input("A :"))
+b=int(input("B :"))
+if n%2==0 and a+b<=n/2:
       print "yes"
 else:
       print "no"
